87c06437311c7489c7e2c6f19118522e77885757
Shubh-debug <shubahm12345552@gmail.com>
2025-09-18 02:18:40 +0530

    Added Frontend hosted url in allowed origin

--- app/main.py
+++ app/main.py
@@ -42,6 +42,7 @@ app.add_middleware(
         "http://127.0.0.1:5173",
         "http://localhost:3000",
         "http://127.0.0.1:3000",
+        "https://founders-ai.vercel.app",
     ],
     allow_credentials=True,
     allow_methods=["GET", "POST", "PUT", "DELETE"],
